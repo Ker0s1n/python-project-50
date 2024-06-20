fa915391af3ac984488fcb97ed4f83212a9b0936
chachimin <Chachimin.DV@vibors.ru>
2024-06-20 16:08:43 +0300

    update step3 linter

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -11,19 +11,13 @@ def generate_diff(file1, file2):
     )))
     stack = ['{']
     for key in keys:
-        try:
-            value1 = input1[key]
-        except Exception:
-            value1 = 'error'
-        try:
-            value2 = input2[key]
-        except Exception:
-            value2 = 'error'
-        if value1 == value2 and value1 != 'error':
+        value1 = input1.get(key)
+        value2 = input2.get(key)
+        if value1 == value2 and value1 is not None:
             stack.append(f'    {key}: {value1}')
-        elif value1 != value2 and value1 != 'error':
+        elif value1 != value2 and value1 is not None:
             stack.append(f'  - {key}: {value1}')
-            if value2 != 'error':
+            if value2 is not None:
                 stack.append(f'  + {key}: {value2}')
         else:
             stack.append(f'  + {key}: {value2}')

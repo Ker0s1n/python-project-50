54b5d5020b75050e30c9e3a24ecbd6e9ffbe33db
Дмитрий Чачимин <chachimin@gmail.com>
2024-06-23 20:22:56 +0300

    modified:   tests/fixtures/result_main.txt 	modified:   tests/test_json_merge.py

--- tests/test_json_merge.py
+++ tests/test_json_merge.py
@@ -1,19 +1,32 @@
+import pytest
 from gendiff.scripts.gendiff import generate_diff, main
 
 
-def test_correct_merge():
+@pytest.fixture
+def paths():
+    return {
+        'first_file': 'tests/fixtures/one.json',
+        'second_file': 'tests/fixtures/two.json',
+        'third_file': 'tests/fixtures/three.json',
+        'first_result': 'tests/fixtures/result_1.txt',
+        'second_result': 'tests/fixtures/result.txt',
+        'third_result': 'tests/fixtures/result_main.txt'
+    }
+
+
+def test_correct_merge(paths):
     assert generate_diff(
-        'tests/fixtures/one.json',
-        'tests/fixtures/two.json'
-    ) == open('tests/fixtures/result_1.txt').read()
+        paths['first_file'],
+        paths['second_file']
+    ) == open(paths['first_result']).read()
 
     assert generate_diff(
-        'tests/fixtures/one.json',
-        'tests/fixtures/three.json'
-    ) == open('tests/fixtures/result.txt').read()
+        paths['first_file'],
+        paths['third_file']
+    ) == open(paths['second_result']).read()
 
 
-def test_main():
+def test_main(paths):
     assert main(
-        ['file1.json', 'file2.json']
-    ) == open('tests/fixtures/result_main.txt').read()
+        [paths['second_file'], paths['third_file']]
+    ) == open(paths['third_result']).read()
